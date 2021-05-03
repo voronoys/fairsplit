e4d6fd5760d7f3f0f5353391b941f7ba8d52d017
lgsilvaesilva <lgsilvaesilva@gmail.com>
2021-05-03 14:56:32 -0300

    new template

---
+++ apptest/server.R
@@ -0,0 +1,38 @@
+################################################################################
+# Server logic of the Shiny app
+#
+# Author: Stefan Schliebs
+# Created: 2020-03-13 09:21:18
+################################################################################
+
+
+server <- function(input, output, session) {
+
+  # # load data model
+  # d_data_model <- reactive({ readRDS("data/app-data-model.rds") })
+  #
+  # # add server logic for count icons
+  # callModule(count_icon, "blog_counter", d_data_model, "n_blogs")
+  # callModule(count_icon, "author_counter", d_data_model, "n_authors")
+  # callModule(count_icon, "word_counter", d_data_model, "n_words")
+  # callModule(count_icon, "page_counter", d_data_model, "n_book_pages")
+  #
+  # # add server logic for summary stats
+  # callModule(insights_charts, "insights_charts", d_data_model)
+  #
+  # # add server logic for the topic graph
+  # callModule(topics_graph, "topics_graph", d_data_model)
+  #
+  # # add server logic for the topic table
+  # callModule(topics_table, "topics_table", d_data_model)
+  #
+  # output$last_refresh <- renderUI({
+  #   last_refresh_formatted <- strftime(d_data_model()$last_refresh, format = "%d %b %Y")
+  #
+  #   HTML(glue("Last data refresh occurred on <strong>{ last_refresh_formatted }</strong>."))
+  # })
+  #
+  #
+  # output$sometest <- renderText(input$mybutton)
+
+}
---
+++ apptest/ui.R
@@ -0,0 +1,35 @@
+################################################################################
+# UI of the Shiny app
+#
+# Author: Stefan Schliebs
+# Created: 2020-03-04 09:34:50
+################################################################################
+
+# https://unsplash.com/photos/WcV2YkM3Dls?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+htmlTemplate(
+  filename = "www/index.html",
+  header_info = textInput('id01', 'Olá')
+  # blog_counter = count_icon_ui("blog_counter", icon = "icon-browser", icon_text = "Articles"),
+  # author_counter = count_icon_ui("author_counter", icon = "icon-profile-female", icon_text = "Authors"),
+  # word_counter = count_icon_ui("word_counter", icon = "icon-search", icon_text = "Words"),
+  # page_counter = count_icon_ui("page_counter", icon = "icon-book-open", icon_text = "Equivalent Book Pages"),
+  # insights_charts = insights_charts_ui("insights_charts"),
+  # last_refresh = uiOutput("last_refresh"),
+  # topics_graph = topics_graph_ui("topics_graph"),
+  # topics_table = topics_table_ui("topics_table")
+
+  # box_packages_new_month = pretty_value_box_ui("packages-new-month", icon_name = "cubes"),
+  # box_packages_updated_month = pretty_value_box_ui("packages-updated-month", background_color = "#7ab885", icon_name = "cubes"),
+  #
+  # box_packages_new_year = pretty_value_box_ui("packages-new-year", icon_name = "cubes"),
+  # box_packages_updated_year = pretty_value_box_ui("packages-updated-year", background_color = "#7ab885", icon_name = "cubes"),
+  #
+  # header_ui = uiOutput("header_ui") %>% withSpinner(size = 0.5, proxy.height = "50px", type = 6, color = "#FFFFFF"),
+  #
+  # package_chart = package_chart_ui("package_chart"),
+  #
+  # featured_packages = featured_packages_ui("featured_packages"),
+  #
+  # dependency_network = graph_network_ui("dependency_network")
+)
